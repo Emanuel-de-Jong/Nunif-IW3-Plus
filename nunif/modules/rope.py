5866c6da3ccb69168f1befeb74f5f5dd352b2600
nagadomi <nagadomi@gmail.com>
2026-06-26 15:30:35 +0900

    Optimize RoPE2d

--- nunif/modules/rope.py
+++ nunif/modules/rope.py
@@ -2,6 +2,28 @@ import torch
 import torch.nn as nn
 
 
+def rotate_half(x: torch.Tensor) -> torch.Tensor:
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2 :]
+    return torch.cat((-x2, x1), dim=-1)
+
+
+def apply_rope(x: torch.Tensor, cos: torch.Tensor, sin: torch.Tensor) -> torch.Tensor:
+    return x * cos + rotate_half(x) * sin
+
+
+def apply_rope_inplace(x: torch.Tensor, cos: torch.Tensor, sin: torch.Tensor) -> torch.Tensor:
+    out = torch.empty_like(x)
+    half = x.shape[-1] // 2
+    i1 = (Ellipsis, slice(None, half))
+    i2 = (Ellipsis, slice(half, None))
+    sin = sin[i1]  # sin[i1] == sin[i2]
+    cos = cos[i1]
+    out[i1] = x[i1] * cos - x[i2] * sin
+    out[i2] = x[i2] * cos + x[i1] * sin
+    return out
+
+
 class RoPE2d(nn.Module):
     cos_h: torch.Tensor
     sin_h: torch.Tensor
@@ -11,32 +33,30 @@ class RoPE2d(nn.Module):
     def __init__(self, head_dim: int, height: int, width: int, theta: float = 100.0) -> None:
         super().__init__()
         assert head_dim % 4 == 0, "head_dim must be a multiple of 4"
+
         self.head_dim = head_dim
         self.theta = theta
         self.height = height
         self.width = width
+        self.dim_chunk = head_dim // 2
 
-        cos_h, sin_h, cos_w, sin_w = self._precompute_freqs(head_dim, height, width)
+        cos_h, sin_h, cos_w, sin_w = self._precompute_freqs()
         self.register_buffer("cos_h", cos_h, persistent=False)
         self.register_buffer("sin_h", sin_h, persistent=False)
         self.register_buffer("cos_w", cos_w, persistent=False)
         self.register_buffer("sin_w", sin_w, persistent=False)
 
     @torch.no_grad()
-    def _precompute_freqs(self, head_dim: int, h: int, w: int):
-        dim_h = head_dim // 2
-        dim_w = head_dim // 2
+    def _precompute_freqs(self):
+        cos_h, sin_h = self._get_1d_sin_cos(self.height, self.dim_chunk)
+        cos_w, sin_w = self._get_1d_sin_cos(self.width, self.dim_chunk)
 
-        cos_h_1d, sin_h_1d = self._get_1d_sin_cos(h, dim_h)
-        cos_w_1d, sin_w_1d = self._get_1d_sin_cos(w, dim_w)
+        cos_h = cos_h.reshape(1, 1, self.height, 1, self.dim_chunk)
+        sin_h = sin_h.reshape(1, 1, self.height, 1, self.dim_chunk)
+        cos_w = cos_w.reshape(1, 1, 1, self.width, self.dim_chunk)
+        sin_w = sin_w.reshape(1, 1, 1, self.width, self.dim_chunk)
 
-        cos_h = cos_h_1d[:, None].repeat(1, w, 1).reshape(1, 1, h * w, dim_h)
-        sin_h = sin_h_1d[:, None].repeat(1, w, 1).reshape(1, 1, h * w, dim_h)
-
-        cos_w = cos_w_1d[None, :].repeat(h, 1, 1).reshape(1, 1, h * w, dim_w)
-        sin_w = sin_w_1d[None, :].repeat(h, 1, 1).reshape(1, 1, h * w, dim_w)
-
-        return cos_h.contiguous(), sin_h.contiguous(), cos_w.contiguous(), sin_w.contiguous()
+        return cos_h, sin_h, cos_w, sin_w
 
     def _get_1d_sin_cos(self, max_len: int, dim: int) -> tuple[torch.Tensor, torch.Tensor]:
         exp = torch.arange(0, dim, 2, dtype=torch.float32) / dim
@@ -46,45 +66,68 @@ class RoPE2d(nn.Module):
         emb = torch.cat([freqs, freqs], dim=-1)
         return emb.cos(), emb.sin()
 
-    @staticmethod
-    def rotate_half(x: torch.Tensor) -> torch.Tensor:
-        assert x.shape[-1] % 2 == 0
-        x1 = x[..., : x.shape[-1] // 2]
-        x2 = x[..., x.shape[-1] // 2 :]
-        return torch.cat((-x2, x1), dim=-1)
-
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         x: [B, num_heads, N, head_dim] (N = h * w)
         """
-        assert x.shape[-1] == self.head_dim
-        x_float = x.to(torch.float32)
-        x_h = x_float[..., : self.head_dim // 2]
-        x_w = x_float[..., self.head_dim // 2 :]
+        B, num_heads, N, head_dim = x.shape
 
-        out_h = (x_h * self.cos_h) + (self.rotate_half(x_h) * self.sin_h)
-        out_w = (x_w * self.cos_w) + (self.rotate_half(x_w) * self.sin_w)
+        x_float = x.float()
 
+        x_h = x_float[..., :self.dim_chunk].reshape(B, num_heads, self.height, self.width, self.dim_chunk)
+        x_w = x_float[..., self.dim_chunk:].reshape(B, num_heads, self.height, self.width, self.dim_chunk)
+
+        if False:
+            # slow
+            out_h = apply_rope(x_h, self.cos_h, self.sin_h)
+            out_w = apply_rope(x_w, self.cos_w, self.sin_w)
+        else:
+            out_h = apply_rope_inplace(x_h, self.cos_h, self.sin_h)
+            out_w = apply_rope_inplace(x_w, self.cos_w, self.sin_w)
+
+        out_h = out_h.reshape(B, num_heads, N, self.dim_chunk)
+        out_w = out_w.reshape(B, num_heads, N, self.dim_chunk)
         return torch.cat([out_h, out_w], dim=-1).to(x.dtype)
 
 
-def _test():
+def _bench(do_compile):
+    import time
+
+    device = "cuda:0"
+    N = 20
+    LAYERS = 10
     B = 4
+    S = (128, 128)
+    dim = 256
     num_heads = 4
-    h = 8
-    w = 4
-    head_dim = 32
+    head_dim = dim // 4
 
-    rope = RoPE2d(head_dim, h, w).cuda()
-    assert rope.cos_w.dtype == torch.float32
-    x = torch.rand((B, num_heads, h * w, head_dim)).cuda()
-    with torch.autocast(device_type=x.device.type):
+    rope = RoPE2d(head_dim, S[0], S[1]).cuda()
+    x = torch.rand((4, num_heads, S[0] * S[1], head_dim), dtype=torch.float16, device="cuda")
+    if do_compile:
+        # over 10x faster
+        rope = torch.compile(rope)
+
+    with torch.inference_mode(), torch.autocast(device_type="cuda"):
         z = rope(x)
-    assert x.shape == z.shape
+        print(z.shape, z.dtype)
 
-    z = rope(x.to(torch.float16))
-    assert z.dtype == torch.float16
+    # check backward works
+    rope(x + nn.Parameter(torch.zeros(1)).cuda()).sum().backward()
+
+    # benchmark
+    torch.cuda.synchronize()
+    t = time.time()
+    with torch.inference_mode(), torch.autocast(device_type="cuda"):
+        for _ in range(N):
+            for _ in range(LAYERS):
+                z = rope(x)
+    torch.cuda.synchronize()
+    print(round(1 / ((time.time() - t) / (B * N)), 3), "FPS")
+    max_vram_mb = int(torch.cuda.max_memory_allocated(device) / (1024 * 1024))
+    print(f"GPU Max Memory Allocated {max_vram_mb}MB")
 
 
 if __name__ == "__main__":
-    _test()
+    _bench(do_compile=False)
+    _bench(do_compile=True)
